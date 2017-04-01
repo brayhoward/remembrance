58c17cb67afb2556d4565bf1ffa571544240ea7e
brayhoward <brandonrhoward@gmail.com>
2017-04-01 17:56:40 -0600

    Process args POC.

--- lib/remembrance.ex
+++ lib/remembrance.ex
@@ -1,6 +1,6 @@
 defmodule Remembrance do
   @moduledoc """
-  Documentation for Remembrance.
+  Documentation for Remembrance, a command-line app for setting timers.
   """
 
   @doc """
@@ -8,11 +8,65 @@ defmodule Remembrance do
 
   ## Examples
 
-      iex> Remembrance.hello
-      :world
+      iex> Remembrance.main []
+      {:ok, "0 hr 3 min 0 sec"}
+
+      iex> Remembrance.main [3]
+      {:ok, "0 hr 3 min 0 sec"}
+
+      iex> Remembrance.main [1, 30, 6]
+      {:ok, "1 hr 30 min 6 sec"}
+
+      iex> Remembrance.main [0, 5, 30]
+      {:ok, "0 hr 5 min 30 sec"}
+
+      // Fucntionaly works but fails when ran as a test.
+      // TODO: debug later.
+      Remembrance.main [1, 3]
+      {:ok, "1 hr 3 min 0 sec"}
 
   """
-  def main(_ \\ []) do
-    IO.puts "Hello world"
+  def main(args) do
+    args
+    |> parse_args
+    |> process
+  end
+
+  #
+  ## Private AF
+  #
+
+  defp process(%{hr: hr, min: min, sec: sec}) do
+    time = "#{hr} hr #{min} min #{sec} sec";
+    IO.puts "Timer set for #{time}\n"
+
+    if min === 3 && hr === 0 && sec === 0 do
+      IO.puts "Don't oversteep that tea! 🍵"
+    end
+
+    {:ok, time}
+  end
+
+  defp parse_args(args) do
+
+    case length args do
+      0 ->
+        IO.puts "No arguments given"
+        # Default to 3 min if no args passed
+        parse_args [3]
+
+      1 ->
+        %{hr: 0, min: List.first(args), sec: 0}
+
+      2 ->
+        [ hr | min ] = args
+        %{hr: hr, min: min, sec: 0}
+
+      _ ->
+        [hr | tail] = args
+        [min | tail] = tail
+        [sec | _] = tail
+        %{hr: hr, min: min, sec: sec}
+    end
   end
 end
